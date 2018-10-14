782ddbf32573e5cc079d15f34c84d7559b35648c
Alexander George Legaspi <lex83nyc@gmail.com>
2018-10-14 16:38:51 -0400

    new credit page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,31 @@
 Rails.application.routes.draw do
+
   devise_for :admins, controllers: {
     registrations: 'admins/registrations',
     sessions: 'admins/sessions'
   }
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'home#index'
 
-  # this rources has a nested resource(join table) with limited views
+  # this rources has a nested resource(join table) with limited routes
   resources :students do
     resources :student_cohorts, only: [:new, :create]
   end
 
   resources :student_cohorts, only: [:destroy]
+
   resources :instructors
 
-  # this rources has a nested resource with limited views
+  # this rources has a nested resource(join table) with limited routes
   resources :courses do
     resources :cohorts, only: [:new, :create]
   end
 
   resources :cohorts, only: [:index, :show, :destroy]
   
-  # this resource is only displaying the index 
+  # this resource for dashboard only has an index route
   resources :dashboard, only: [:index]
+
+  
 end
